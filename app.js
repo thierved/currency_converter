b6f7240bf20f413652fab6adf5fa39f7e1b4ae44
Thierno Sadou Diallo <thierved01@gmail.com>
2018-06-27 12:50:10 +0000

    I finished the online version of the app

--- app.js
+++ app.js
@@ -1,32 +1,46 @@
-const from = 'USD';
-const to = 'XOF';
-
 const select_left = document.getElementById('select-left');
 const select_right = document.getElementById('select-right');
 const input_left = document.getElementById('input-left');
 const input_right = document.getElementById('input-right');
-const fromTo = from+"_"+to;
+const convert_button = document.getElementById('convert');
+const user_input = document.getElementById('input');
+let from ;
+let to ;
 
 
-let url_convert = "https://free.currencyconverterapi.com/api/v5/convert?q="+fromTo;
-const url_countries = 'https://free.currencyconverterapi.com/api/v5/countries';
 
-fetch(url_countries).then(response => response.json())
-                     .then(({results}) => {
-                        let currency = ''; 
-                        for(o in results) {
-                            currency += `<option>${o}</option>`;
-                        }
-                        select_left.innerHTML = currency;
-                        select_right.innerHTML = currency;                        
-                     });
+const url_currencies = 'https://free.currencyconverterapi.com/api/v5/currencies';
 
-fetch(url_convert).then(res => res.json())
-                    .then(({results}) => {
-                        const ratio = results[fromTo].val;
-                        input_left.value = 2;
-                        input_right.value = input_left.value * ratio;
-                    });
+fetch(url_currencies)
+    .then(response => response.json())
+        .then(({results}) => {
+        let currency = ''; 
+        for(o in results) {
+            currency += `<option>${o}</option>`;
+        }
+        select_left.innerHTML = currency;
+        select_right.innerHTML = currency;                        
+    });
+
+convert_button.addEventListener('click', () => {
+    input_left.value = "...";
+    input_right.value = "...";
+
+    from = select_left.options[select_left.selectedIndex].text;
+    to = select_right.options[select_right.selectedIndex].text;
+    const fromTo = from+"_"+to;
+
+    let url_convert = "https://free.currencyconverterapi.com/api/v5/convert?q="+fromTo;
+    fetch(url_convert)
+        .then(res => res.json())
+            .then(({results}) => {
+                const ratio = results[fromTo].val;
+                input_left.value = user_input.value;
+                input_right.value = input_left.value * ratio;
+        });
+
+});
+
 
 
 
