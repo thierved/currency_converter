8976b4a0fef056ea36eb0386c7eca3b908de96f1
Thierno Sadou Diallo <thierved01@gmail.com>
2018-06-27 19:38:28 +0000

    I have fixed the problem of select options

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const input_left = document.getElementById('input-left');
 const input_right = document.getElementById('input-right');
 const convert_button = document.getElementById('convert');
 const user_input = document.getElementById('input');
-let from ;
-let to ;
+
+let from, to ;
+let moneyCode = {};
 
 
 
@@ -14,9 +15,11 @@ const url_currencies = 'https://free.currencyconverterapi.com/api/v5/currencies'
 fetch(url_currencies)
     .then(response => response.json())
         .then(({results}) => {
-        let currency = ''; 
+        let currency = '';         
+        
         for(o in results) {
-            currency += `<option>${o}</option>`;
+            currency += `<option>${results[o].currencyName}</option>`;
+            moneyCode[results[o].currencyName] = o;
         }
         select_left.innerHTML = currency;
         select_right.innerHTML = currency;                        
@@ -26,8 +29,8 @@ convert_button.addEventListener('click', () => {
     input_left.value = "...";
     input_right.value = "...";
 
-    from = select_left.options[select_left.selectedIndex].text;
-    to = select_right.options[select_right.selectedIndex].text;
+    from = moneyCode[select_left.options[select_left.selectedIndex].text];
+    to = moneyCode[select_right.options[select_right.selectedIndex].text];
     const fromTo = from+"_"+to;
 
     let url_convert = "https://free.currencyconverterapi.com/api/v5/convert?q="+fromTo;
